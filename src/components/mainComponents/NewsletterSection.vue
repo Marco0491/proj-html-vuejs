124463b750646831208adbc6ca0abc0110440c5b
Marco Crisostomi <m.crisostomi91@gmail.com>
2022-04-11 17:48:30 +0200

    add newsletter section

--- src/components/mainComponents/NewsletterSection.vue
+++ src/components/mainComponents/NewsletterSection.vue
@@ -1,5 +1,20 @@
 <template>
-    <section>
+    <section class="container-fluid position-relative p-5">
+        <div class="row flex-column align-items-center">
+            <div class="my_circle position-absolute"></div>
+            <div class="col-4 text-center p-4">
+                <h1>Subscribe <span class="my_green fw-light">Newsletters</span></h1>
+                <p class="my_gray fw-light">Enter your email address to register to our newsletter subscription delivered on a regular basis!</p>
+            </div>
+            <div class="col-4">
+                <div class="input-group mb-3">
+                    <input type="email" class="form-control bg-light my_gray fw-light" placeholder="Enter your email">
+                    <span class="input-group-text text-white fw-bolder px-3 py-2" id="basic-addon2">Subscribe</span>
+                </div>
+            </div>
+            <img class="first-shape position-absolute" src="../../assets/images/maxcoach-shape-09.png" alt="maxcoach shape">
+            <img class="second-shape position-absolute" src="../../assets/images/maxcoach-shape-02.png" alt="maxcoach shape">
+        </div>
     </section>
 </template>
 
@@ -10,4 +25,38 @@ name: 'NewsletterSection',
 </script>
 
 <style lang="scss" scoped>
+@import '../../assets/styles/variables.scss';
+
+    div.my_circle {
+        @include green-circle;
+        top: 50%;
+        left: 15%;
+        filter: opacity(0.5);
+    }
+
+    p {
+        font-size: 0.8rem;
+        line-height: 1.5rem;
+    }
+
+    input {
+        font-size: 0.7rem;
+        border: transparent;
+    }
+
+    span#basic-addon2 {
+        font-size: 0.7rem;
+        background-color: $brand_green;
+    }
+    
+    img.first-shape {
+        width: 175px;
+        right: 20%;
+    }
+
+    img.second-shape {
+        width: 120px;
+        top: 40%;
+        right: 20%;
+    }
 </style>
